796fb7534264486be15fdcf597ef91989faee663
Tomas Santana <tomas.30604530@uru.edu>
2025-04-23 20:10:42 -0500

    change callback for testing

--- DevilCV/main.py
+++ DevilCV/main.py
@@ -18,28 +18,29 @@ def main():
     stream = Stream(source=0, exposure=-6, multi_detectors=[multi_detector], show=True)
 
     def callback(center_detections: Detections):
-        red_center_x = center_detections['MultiColorDetector']['Red'][0].center[0] if 'Red' in center_detections['MultiColorDetector'] else None
-        red_edge_x = center_detections['MultiColorDetector']['Red'][0].bounding_box[0] if 'Red' in center_detections['MultiColorDetector'] else None
-        blue_center_x = center_detections['MultiColorDetector']['Blue'][0].center[0] if 'Blue' in center_detections['MultiColorDetector'] else None
-        blue_edge_x = center_detections['MultiColorDetector']['Blue'][0].bounding_box[0] if 'Blue' in center_detections['MultiColorDetector'] else None
+        # red_center_x = center_detections['MultiColorDetector']['Red'][0].center[0] if 'Red' in center_detections['MultiColorDetector'] else None
+        # red_edge_x = center_detections['MultiColorDetector']['Red'][0].bounding_box[0] if 'Red' in center_detections['MultiColorDetector'] else None
+        # blue_center_x = center_detections['MultiColorDetector']['Blue'][0].center[0] if 'Blue' in center_detections['MultiColorDetector'] else None
+        # blue_edge_x = center_detections['MultiColorDetector']['Blue'][0].bounding_box[0] if 'Blue' in center_detections['MultiColorDetector'] else None
         
-        red_center_x, _ = top_left_to_center_relative(red_center_x, 0, stream.resolution) if red_center_x is not None else (None, None)
-        red_edge_x, _ = top_left_to_center_relative(red_edge_x, 0, stream.resolution) if red_edge_x is not None else (None, None)
-        blue_center_x, _ = top_left_to_center_relative(blue_center_x, 0, stream.resolution) if blue_center_x is not None else (None, None)
-        blue_edge_x, _ = top_left_to_center_relative(blue_edge_x, 0, stream.resolution) if blue_edge_x is not None else (None, None)
+        # red_center_x, _ = top_left_to_center_relative(red_center_x, 0, stream.resolution) if red_center_x is not None else (None, None)
+        # red_edge_x, _ = top_left_to_center_relative(red_edge_x, 0, stream.resolution) if red_edge_x is not None else (None, None)
+        # blue_center_x, _ = top_left_to_center_relative(blue_center_x, 0, stream.resolution) if blue_center_x is not None else (None, None)
+        # blue_edge_x, _ = top_left_to_center_relative(blue_edge_x, 0, stream.resolution) if blue_edge_x is not None else (None, None)
 
-        # send only non-null values to the bridge
-        values = {}
-        if red_center_x is not None:
-            values['vision/color/red/center_x'] = red_center_x
-        if red_edge_x is not None:
-            values['vision/color/red/edge_x'] = red_edge_x
-        if blue_center_x is not None:
-            values['vision/color/blue/center_x'] = blue_center_x
-        if blue_edge_x is not None:
-            values['vision/color/blue/edge_x'] = blue_edge_x
+        # # send only non-null values to the bridge
+        # values = {}
+        # if red_center_x is not None:
+        #     values['vision/color/red/center_x'] = red_center_x
+        # if red_edge_x is not None:
+        #     values['vision/color/red/edge_x'] = red_edge_x
+        # if blue_center_x is not None:
+        #     values['vision/color/blue/center_x'] = blue_center_x
+        # if blue_edge_x is not None:
+        #     values['vision/color/blue/edge_x'] = blue_edge_x
+        print(center_detections)
 
-        bridge.set_values(values)
+        # bridge.set_values(values)
 
     stream.start(callback, record=True)
 
